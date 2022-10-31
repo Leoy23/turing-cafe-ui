1947c289e076ae99bf1744eecd5a006b093c96a7
Leah Young <leah.young21@alumni.colostate.edu>
2022-10-31 10:03:38 -0600

    create resy card structure and import/export

---
+++ src/components/ReservationCards/ReservationCards.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './ReservationCards.css'
+
+const ReservationCards = ({id, name, date, time, number}) => {
+    return (
+        <section className='card'>
+            <p>{name}</p>
+            <p>{date}</p>
+            <p>{time} pm</p>
+            <p>Number of Guests:{number}</p>
+            <button>Cancel</button>
+        </section>
+    )
+}
+
+export default ReservationCards;
