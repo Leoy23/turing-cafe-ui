04856195667383486b47ce3bdc882b5e80841961
Leah Young <leah.young21@alumni.colostate.edu>
2022-10-31 10:03:07 -0600

    created allResys variable to map over/create resy cards

--- src/components/Reservations/Reservations.js
+++ src/components/Reservations/Reservations.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import ReservationCards from './../ReservationCards/ReservationCards'
+import './Reservations.css'
 
-const Reservations = () => {
+const Reservations = ({reservations}) => {
+    const allResys = reservations.map(resy => {
+        return (
+            <ReservationCards 
+                id={resy.id}
+                name={resy.name}
+                date={resy.date}
+                time={resy.time}
+                number={resy.number}
+                key={resy.id}
+            />
+        )
+    })
+
     return(
         <section className='reservations-container'>
-            <h1>RESERVATIONS GO HERE</h1>
+            {allResys}
         </section>
     )
 }
