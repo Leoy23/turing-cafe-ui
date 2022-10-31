4ba3adb3b79b15f3f699e374ee77bcc6e75f2a33
Leah Young <leah.young21@alumni.colostate.edu>
2022-10-31 09:52:05 -0600

    add imports and exports for Reservations comp

---
+++ src/components/Reservations/Reservations.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Reservations = () => {
+    return(
+        <section className='reservations-container'>
+            <h1>RESERVATIONS GO HERE</h1>
+        </section>
+    )
+}
+
+export default Reservations;
